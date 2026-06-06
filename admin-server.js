79aee832e5dd9a9b4ccde72075b2a8ceea61b9dd
ankit <ankitk538@gmail.com>
2026-06-06 20:24:45 +0530

    Rename landing.html->index.html, index.html->verify.html for Vercel routing; update all internal links

--- admin-server.js
+++ admin-server.js
@@ -577,7 +577,7 @@ app.use((req, res, next) => {
 
 // Serve landing page as the default homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "landing.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/robots.txt", (req, res) => {
